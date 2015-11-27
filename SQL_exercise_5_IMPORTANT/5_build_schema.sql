353f1e15f4a3bf1fbd9f1e33b2cdaaddf9dd0e6f
XD-DENG <xd_deng@hotmail.com>
2015-11-27 11:28:50 +0800

    A new exercise, including a quite interesting query

---
+++ SQL_exercise_5_IMPORTANT/5_build_schema.sql
@@ -0,0 +1,40 @@
+-- https://en.wikibooks.org/wiki/SQL_Exercises/Pieces_and_providers
+
+CREATE TABLE Pieces (
+ Code INTEGER PRIMARY KEY NOT NULL,
+ Name TEXT NOT NULL
+ );
+CREATE TABLE Providers (
+ Code VARCHAR(40) 
+ PRIMARY KEY NOT NULL,  
+ Name TEXT NOT NULL 
+ );
+CREATE TABLE Provides (
+ Piece INTEGER, 
+ FOREIGN KEY (Piece) REFERENCES Pieces(Code),
+ Provider VARCHAR(40), 
+ FOREIGN KEY (Provider) REFERENCES Providers(Code),  
+ Price INTEGER NOT NULL,
+ PRIMARY KEY(Piece, Provider) 
+ );
+ 
+ 
+ 
+ INSERT INTO Providers(Code, Name) VALUES('HAL','Clarke Enterprises');
+INSERT INTO Providers(Code, Name) VALUES('RBT','Susan Calvin Corp.');
+INSERT INTO Providers(Code, Name) VALUES('TNBC','Skellington Supplies');
+
+INSERT INTO Pieces(Code, Name) VALUES(1,'Sprocket');
+INSERT INTO Pieces(Code, Name) VALUES(2,'Screw');
+INSERT INTO Pieces(Code, Name) VALUES(3,'Nut');
+INSERT INTO Pieces(Code, Name) VALUES(4,'Bolt');
+
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'HAL',10);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'HAL',20);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'TNBC',14);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'RBT',50);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'TNBC',45);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'HAL',5);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'RBT',7);
---
+++ SQL_exercise_5_IMPORTANT/5_questions_and_solutions.sql
@@ -0,0 +1,114 @@
+-- https://en.wikibooks.org/wiki/SQL_Exercises/Pieces_and_providers
+
+show tables;
+
+select * from Pieces;
+select * from Providers;
+select * from provides; 
+
+
+-- Select the name of all the pieces. 
+select Name from Pieces;
+-- 
+-- 
+-- Select all the providers' data. 
+select * from providers;
+-- 
+-- 
+-- Obtain the average price of each piece (show only the piece code and the average price).
+select piece, avg(price) 
+from Provides 
+group by piece;
+-- 
+-- 
+-- Obtain the names of all providers who supply piece 1.
+select Name 
+from Providers
+where Code in (
+select  Provider from provides where Piece = 1
+); 
+
+select Providers.Name 
+from Providers join Provides
+on Providers.Code = Provides.Provider
+where Provides.Piece = 1;
+
+/* Without subquery */
+ SELECT Providers.Name
+   FROM Providers INNER JOIN Provides
+          ON Providers.Code = Provides.Provider
+             AND Provides.Piece = 1;
+             
+/* With subquery */
+ SELECT Name
+   FROM Providers
+  WHERE Code IN
+   (SELECT Provider FROM Provides WHERE Piece = 1);
+
+-- 
+-- Select the name of pieces provided by provider with code "HAL".
+select Name from Pieces
+where Code in (
+select Piece from Provides where Provider = 'HAL'
+);
+
+select Pieces.Name
+from Pieces join Provides
+on (Pieces.code = Provides.Piece)
+where Provides.Provider = 'HAL';
+
+/* With EXISTS subquery */   
+-- Interesting clause
+SELECT Name
+  FROM Pieces
+  WHERE EXISTS
+  (
+    SELECT * FROM Provides
+      WHERE Provider = 'HAL'
+        AND Piece = Pieces.Code
+  );
+
+-- ---------------------------------------------
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- Intereting and important one.
+-- For each piece, find the most expensive offering of that piece and include the piece name, provider name, and price 
+-- (note that there could be two providers who supply the same piece at the most expensive price).
+
+-- Wrong solution given by XD-DENG. 
+select a.name, a.code, b.price, c.Name
+from Pieces a join Provides b
+on a.Code = b.Piece
+join Providers c
+on b.provider = c.Code
+group by a.code;
+-- this is wrong since when I group by a.code, SQL will automatically select the first c.Name in each group to return,
+-- which is not what we expected.
+
+-- CORRECT SOLUTION
+SELECT Pieces.Name, Providers.Name, Price
+  FROM Pieces INNER JOIN Provides ON Pieces.Code = Piece
+              INNER JOIN Providers ON Providers.Code = Provider
+  WHERE Price =
+  (
+    SELECT MAX(Price) FROM Provides
+    WHERE Piece = Pieces.Code
+  );
+-- This is worthwhile to look into again
+-- --------------------------------------------------------------
+ 
+-- Add an entry to the database to indicate that "Skellington Supplies" (code "TNBC") will provide sprockets (code "1") for 7 cents each.
+insert into Provides(Piece, Provider, Price)
+values (1, 'TNBC', 7);
+
+-- Increase all prices by one cent.
+update Provides
+set Price = Price + 1;
+
+-- Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply bolts (code 4).
+delete from Provides where provider = 'RBT' AND Piece = 4;
+
+
+-- Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply any pieces 
+-- (the provider should still remain in the database).
+delete from provides
+where Provider = 'RBT';
