fbace073806c9950c1593a8c5c5875e478db3dad
Xiaodong <XD-DENG@users.noreply.github.com>
2016-10-03 14:51:19 +0800

    Update 4_questions_and_solutions.sql

--- SQL_exercise_4_IMPORTANT/4_questions_and_solutions.sql
+++ SQL_exercise_4_IMPORTANT/4_questions_and_solutions.sql
@@ -7,7 +7,6 @@ select title from movies;
 -- 4.2
 -- Show all the distinct ratings in the database.
 select distinct rating from movies;
-select * from movies;
 
 -- 4.3 
 -- Show all unrated movies.
@@ -25,7 +24,8 @@ where Movie is NULL;
 -- and, additionally, the data from the movie that is being shown in the theater (if one is being shown).
 
 -- This query below would fail as it will only return the theaters with movies shown.
--- we need to use left outer join instead
+-- we need to use left outer join instead.
+-- This is a great example to demonstrate why we need to use left join rather than inner join sometimes.
 select * 
 from MovieTheaters join Movies 
 on MovieTheaters.movie = Movies.code;
@@ -48,12 +48,16 @@ on movies.code = movietheaters.movie;
  SELECT *
    FROM MovieTheaters RIGHT JOIN Movies
    ON MovieTheaters.Movie = Movies.Code;
-
+ -- or 
+ SELECT *
+   FROM Movies LEFT JOIN MovieTheaters
+   ON Movies.Code = MovieTheaters.Movie;
 
 -- 4.7
 -- Show the titles of movies not currently being shown in any theaters.
--- VERY IMPORTANT
--- the query below would fail due to the NULL value returned by the subquery
+-- VERY IMPORTANT!!!
+
+-- the query below would FAIL due to the NULL value returned by the subquery
 select title 
 from movies
 where code not in ( 
@@ -77,17 +81,15 @@ select movie from movietheaters
 
  -- 4.8
 -- Add the unrated movie "One, Two, Three".
-insert into Movies(Title) values('One, Two, Three');
 
 INSERT INTO Movies(Title,Rating) VALUES('One, Two, Three',NULL);
 
-select * from Movies;
 
 -- 4.9
 -- Set the rating of all unrated movies to "G".
-update Movies
-set Rating = 'G'
-where Rating is NULL;
+UPDATE Movies
+SET Rating = 'G'
+WHERE Rating is NULL;
 
 -- 4.10
 -- Remove movie theaters projecting movies rated "NC-17".
