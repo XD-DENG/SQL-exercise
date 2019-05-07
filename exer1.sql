254efd7b54b211e1b317b2f8a151820c066555c7
linjiejun <502964379@qq.com>
2019-05-07 15:05:50 +0800

    远程无法登陆到的修复脚本

--- exer1.sql
+++ exer1.sql
@@ -7,3 +7,6 @@ SHOW TABLES ;#进入到某个数据库，显示数据库到所有表。
 
 SELECT DATABASE();#当前选中数据库
 SELECT USER();#当前用户;ROOT@218.66.48.229--用户名以及其客户端的HOST地址。
+
+
+update user set Host = '%' where User='root';#远程root用户不能够登陆到数据库服务器到解决办法，就是允许所有到ip都可以远程登陆
